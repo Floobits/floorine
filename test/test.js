e458bd16cd7dffafd44def3a8715ad0f1ead29f6
Geoff Greer <geoff@greer.fm>
2014-01-02 15:04:28 -0800

    Jslint.

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@
 
 var log = require('../lib/log');
 
-log.set_log_level("debug")
+log.set_log_level("debug");
 log.log("asdf");
 log.debug("qwe");
 log.warn("qwe");
 
-try{
+try {
   throw new Error("asdf");
 } catch (e) {
   log.error(e);
