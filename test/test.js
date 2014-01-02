a2a958a12ba95e231228cb481077b20b41888f9b
Matt Kaniaris <mkaniaris@gmail.com>
2014-01-02 15:00:28 -0800

    new formating.  Kill extra info, add some testing statements

--- test/test.js
+++ test/test.js
@@ -1,1 +1,14 @@
 // TODO: tests
+
+var log = require('../lib/log');
+
+log.set_log_level("debug")
+log.log("asdf");
+log.debug("qwe");
+log.warn("qwe");
+
+try{
+  throw new Error("asdf");
+} catch (e) {
+  log.error(e);
+}
