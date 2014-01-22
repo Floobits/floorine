7578c160dac6919ef40c57f8e15bfe809882029d
Geoff Greer <geoff@greer.fm>
2014-01-22 13:58:06 -0800

    Pass jshint as well as jslint. Print emoji. Don't die if log_level isn't a string. Bump version.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,16 @@
 var log = require('../lib/log');
 
 log.set_log_level("debug");
-log.log("asdf");
-log.debug("qwe");
-log.warn("qwe");
+log.debug("debug");
+log.info("info");
+log.log("log");
+log.warn("warn");
+log.error("error");
 
 try {
   throw new Error("asdf");
 } catch (e) {
+  log.log("Logging exception...");
   log.error(e);
+  log.log("All done!");
 }
