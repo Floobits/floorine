5e5b95ecd149e2e65a6b8ddcc83e9b3f3bb150c0
Geoff Greer <geoff@greer.fm>
2015-02-18 15:25:35 -0800

    Test more functionality.

--- test/test.js
+++ test/test.js
@@ -1,16 +1,37 @@
+/* jshint node: true */
+"use strict";
+
 var log = require('../lib/log');
 
-log.set_log_level("debug");
-log.debug("debug");
-log.info("info");
-log.log("log");
-log.warn("warn");
-log.error("error");
+function test_logging() {
+  log.log("Logging with config", log.config);
+  log.set_log_level("debug");
+  log.debug("debug");
+  log.info("info");
+  log.log("log");
+  log.warn("warn");
+  log.error("error");
 
-try {
-  throw new Error("asdf");
-} catch (e) {
-  log.log("Logging exception...");
-  log.error(e);
+  log.log("Logging an object:", {
+    key: "value",
+    other_key: "other_value",
+  });
+
+  try {
+    throw new Error("asdf");
+  } catch (e) {
+    log.log("Logging an exception...");
+    log.error(e);
+  }
+
   log.log("All done!");
 }
+
+test_logging();
+
+log.set_config({
+  emoji: false,
+  color: false,
+});
+
+test_logging();
